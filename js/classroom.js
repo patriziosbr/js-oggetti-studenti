304bbb7f4443cf72ec7a1fd2b49f5c374c1032fb
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-05-10 15:44:34 +0200

    ClassRoom ok

--- js/classroom.js
+++ js/classroom.js
@@ -1,7 +1,6 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-// Creare un array di oggetti di studenti.
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
 // Dare la possibilità all'utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell'ordine: nome, cognome e età.
 
 //---------STUDENTE------
@@ -9,14 +8,54 @@ var student = {
     nome: "paolo",
     cognome: "di biase",
     eta: 19
-}
+};
 
 for (var key in student) {
-    var nameStud = student.nome;
-    var surName = student.cognome;
-    var age = student.eta;
+    // var nameStud = student.nome;
+    // var surName = student.cognome;
+    // var age = student.eta;
     console.log( key, student[key]);
-}
+};
+
 //---------ARR STUDENTI
+// Creare un array di oggetti di studenti.
+
+var classRoom = [
+    {
+        nome: "maria",
+        cognome: "verdi",
+        eta:24
+    },
+    {
+        nome: "mario",
+        cognome: "neri",
+        eta: 29
+    },
+    {
+        nome: "giuseppe",
+        cognome: "vedi",
+        eta: 39
+    },
+    {
+        nome: "paolo",
+        cognome: "di biase",
+        eta: 49
+    },
+    {
+        nome: "giovanna",
+        cognome: "rossi",
+        eta: 59
+    }
+];
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+for (var i = 0; i < classRoom.length; i++) {
+    var studentName = classRoom[i].nome;
+    var studentSurName = classRoom[i].cognome;
+    console.log(i + 1, studentName, studentSurName);
+};
+
+//-------PROMPT 
+// Dare la possibilità all'utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell'ordine: nome, cognome e età.
 
 
